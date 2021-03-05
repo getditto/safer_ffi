464e4561689161cc79af7414f4eed9ee819d6a36
Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>
2021-03-05 20:06:38 +0100

    Made `repr_c::Vec` be `ReprNApi`

--- src/vec.rs
+++ src/vec.rs
@@ -3,7 +3,7 @@ use ::core::slice;
 use crate::slice::*;
 
 ReprC! {
-    #[repr(C)]
+    #[repr(C, nodejs)]
     #[cfg_attr(all(docs, feature = "nightly"), doc(cfg(feature = "alloc")))]
     /// Same as [`Vec<T>`][`rust::Vec`], but with guaranteed `#[repr(C)]` layout
     pub
