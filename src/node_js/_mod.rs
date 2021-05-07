dd36e72cdfc8414a19caa41e11e85269c3411036
Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>
2021-05-07 15:26:49 +0200

    Node.js – Add idiomatic wrapper for N-API's `env.spawn()`

--- src/node_js/_mod.rs
+++ src/node_js/_mod.rs
@@ -100,3 +100,119 @@ match_! {(
         )*
     );
 }}
+
+#[allow(missing_debug_implementations)]
+pub
+struct JsPromise<ResolvesTo = JsUndefined> /* = */ (
+    JsObject,
+    ::core::marker::PhantomData<ResolvesTo>,
+);
+
+impl<ResolvesTo> NapiValue for JsPromise<ResolvesTo> {
+    unsafe
+    fn from_raw (env: sys::napi_env, value: sys::napi_value)
+      -> Result<Self>
+    {
+        JsObject::from_raw(env, value)
+            .map(|obj| Self(obj, Default::default()))
+    }
+
+    unsafe
+    fn from_raw_unchecked (env: sys::napi_env, value: sys::napi_value)
+      -> Self
+    {
+        Self(JsObject::from_raw_unchecked(env, value), Default::default())
+    }
+
+    unsafe
+    fn raw (self: &'_ Self)
+      -> sys::napi_value
+    {
+        self.0.raw()
+    }
+}
+
+impl<ResolvesTo> JsPromise<ResolvesTo> {
+    pub
+    fn from_task_spawned_on_worker_pool<R, F> (
+        env: &'_ Env,
+        task: F,
+    ) -> Result<JsPromise<ResolvesTo>>
+    where
+        F : 'static + Send + FnOnce() -> R,
+        R : 'static + Send + crate::layout::ReprC,
+        <R as crate::layout::ReprC>::CLayout : ReprNapi<NapiValue = ResolvesTo>,
+    {
+        struct UnsafeAssertSend<T>(T);
+
+        unsafe
+        impl<T> Send for UnsafeAssertSend<T>
+        {}
+
+        struct NapiTask<F> /* = */ (
+            Option<F>,
+        );
+
+        impl<R, F> ::napi::Task for NapiTask<F>
+        where
+            F : 'static + Send + FnOnce() -> R,
+            R : 'static + Send + crate::layout::ReprC,
+            <R as crate::layout::ReprC>::CLayout : ReprNapi,
+        {
+            type Output = UnsafeAssertSend<
+                <R as crate::layout::ReprC>::CLayout
+            >;
+
+            type JsValue =
+                <
+                    <R as crate::layout::ReprC>::CLayout
+                    as
+                    ReprNapi
+                >::NapiValue
+            ;
+
+            fn compute (self: &'_ mut NapiTask<F>)
+              -> Result<Self::Output>
+            {
+                self.0
+                    .take()
+                    .ok_or_else(|| Error::from_reason("\
+                        Attempted to perform the background (worker pool) \
+                        `::napi::Task::compute`-ation more than once!\
+                    ".into()))
+                    .map(|f| f())
+                    .map(|repr_c| unsafe {
+                        crate::layout::into_raw(repr_c)
+                    })
+                    .map(UnsafeAssertSend)
+            }
+
+            fn resolve (
+                self: NapiTask<F>,
+                env: Env,
+                UnsafeAssertSend(output): Self::Output,
+            ) -> Result<Self::JsValue>
+            {
+                output.to_napi_value(&env)
+            }
+        }
+
+        let async_work_promise = env.spawn(NapiTask(Some(task)))?;
+        Ok(JsPromise(async_work_promise.promise_object(), Default::default()))
+    }
+
+    pub
+    fn resolve_into_unknown (self: JsPromise<ResolvesTo>)
+      -> JsPromise<JsUnknown>
+    {
+        JsPromise(self.0, Default::default())
+    }
+
+    pub
+    fn into_unknown (self: JsPromise<ResolvesTo>)
+      -> JsUnknown
+    {
+        self.0
+            .into_unknown()
+    }
+}
