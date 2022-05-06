2e186c64ef413c87ce1c279c5b0ed8a192e6bec6
ronan-ditto <ronan-ditto@users.noreply.github.com>
2022-05-06 07:01:11 +0000

    deploy: d079d25808b869a381200773eb5fc3adf1a14e06

--- rustdoc/source-files.js
+++ rustdoc/source-files.js
@@ -1,7 +1,7 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["cfg_if"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["ctor"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["ghost"] = {"name":"","files":["args.rs","derive.rs","lib.rs","parse.rs","variance.rs","visibility.rs"]};
+sourcesIndex["ghost"] = {"name":"","files":["derive.rs","lib.rs","parse.rs","variance.rs","visibility.rs"]};
 sourcesIndex["inventory"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["inventory_impl"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["libc"] = {"name":"","dirs":[{"name":"unix","dirs":[{"name":"linux_like","dirs":[{"name":"linux","dirs":[{"name":"arch","dirs":[{"name":"generic","files":["mod.rs"]}],"files":["mod.rs"]},{"name":"gnu","dirs":[{"name":"b64","dirs":[{"name":"x86_64","files":["align.rs","mod.rs","not_x32.rs"]}],"files":["mod.rs"]}],"files":["align.rs","mod.rs"]}],"files":["align.rs","mod.rs","non_exhaustive.rs"]}],"files":["mod.rs"]}],"files":["align.rs","mod.rs"]}],"files":["fixed_width_ints.rs","lib.rs","macros.rs"]};
