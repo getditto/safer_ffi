0f83953f2c3fa07c90abea1b0d2bbf0a7037dc41
Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>
2025-03-24 16:05:38 +0100

    Remove deprecated `proc_macros` Cargo feature

--- build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    if cfg!(feature = "proc_macros") {
-        println!("cargo:warning=[safer-ffi] \
-            `proc-macros` feature is deprecated and will be removed \
-            (now built-in)\
-        ");
-    }
-}
